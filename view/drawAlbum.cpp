900d44cd4b5cae7edbe6ee4f201db6ac572a805d
3030 <2103179803@qq.com>
2026-01-31 23:35:19 +0800

    专辑封面绘制的实现

--- view/drawAlbum.cpp
+++ view/drawAlbum.cpp
@@ -1,4 +1,18 @@
 #include "drawAlbum.h"
-void drawAlbum (std::vector<Song> &songs_list,int index) {
+#include <easyx.h>
 
+void drawAlbum (std::vector<Song> &songs_list,int index) {
+    //°²È«¼ì²é
+    if (songs_list.empty()) {
+         return;
+    }
+    if (index<0||index>songs_list.size()) {
+        return;
+    }
+    Song& CurrentSong = songs_list[index];
+    std::string CurrentPath = music_path + CurrentSong.song_name + ".jpg";
+    //std::string CurrentPath = music_path + "¤Á¤Ã¤Á¤ã¤ÊË½" + ".jpg";
+    IMAGE cover_img;
+    loadimage(&cover_img, CurrentPath.c_str(), width_album, length_album);
+    putimage(41, 114, &cover_img);
 }
