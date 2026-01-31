e58f904508a18b1fa67ea301fabbc42ba3d83d06
Astraclicker <2867314230@qq.com>
2026-01-31 13:59:58 +0800

    提交设置界面打印音量图标

--- view/base.h
+++ view/base.h
@@ -8,6 +8,8 @@ ExMessage msg;
 //构建一个功能基础按钮
 inline button base_function(50, 50,WHITE, button_style::roundrect);
 inline button base_function_setting(50,70,0x808080,button_style::roundrect);
+inline button base_vol(40,40,0x808080,button_style::roundrect);
+inline button base_vol_on(40,40,WHITE,button_style::roundrect);
 inline button base_function_setting_on(50,70,WHITE,button_style::roundrect);
 //构建文本类
 inline txt CN(0, 0, 0xd4a74c, "FZCuYuan-M03");
--- view/drewSetting.h
+++ view/drewSetting.h
@@ -9,6 +9,9 @@ button_txt exit_setting_on(190,140,"退出",base_function_setting_on,CN);
 //打开频谱
 button_txt spectrum_setting(190,220,"频谱",base_function_setting,CN);
 button_txt spectrum_setting_on(190,220,"频谱",base_function_setting_on,CN);
+//音量
+button_img vol(300,330,base_vol,"sources/image/audio_modes/btn_volume_on_down.png");
+button_img vol_on(300,330,base_vol_on,"sources/image/audio_modes/btn_volume_on_down.png");
 
 //1.滑块的坐标和点击状态
 int sliderX=470;
@@ -27,8 +30,6 @@ void setVloume(int v) {
 
 //3.画出设置界面
 void drawSetting () {
-    IMAGE image;
-    loadimage(&image,"sources/image/audio_modes/btn_volume_on_down.png",40,40);
     setfillcolor(0x808080);
     BeginBatchDraw();
     solidroundrect(width_window/8,length_window/8,width_window-width_window/8,length_window-length_window/8,10,10);
@@ -46,11 +47,14 @@ void drawSetting () {
         spectrum_setting.drawButton();
     }
 
+    if (vol.checkButton(msg.x,msg.y)) {
+       vol_on.drawButton();
+    }else {
+        vol.drawButton();
+    }
     //画出音乐音量条
     //负责人：凉雨
-    outtextxy(200,330,"音量:");
-
-    putimage(300, 330, &image);  //
+    outtextxy(200,340,"音量:");
     // 绘制音量滑块轨道
     rectangle(350, 350, 550, 355);
 
