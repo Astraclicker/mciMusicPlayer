d5ef3c1c9727bd1430c8d4b44e99c72c9ab8c9b1
Astraclicker <2867314230@qq.com>
2026-01-31 15:03:29 +0800

    提交mci示例，main里的是循环播放的示例

--- main.cpp
+++ main.cpp
@@ -4,12 +4,17 @@ int main() {
 
     //声明音乐路径
     std::string music_address =music_path+"ちっちゃな私.mp3";
-    std::string cmd = "open \"" + music_address + "\" alias " + deviceName;
-    mciSendString(cmd.c_str(), NULL, 0, NULL);
-    //声明播放命令（只要在音乐路径前加一个play 就行注意，注意，注意，这里的play后有一个空格，一定要加！！！）
-    std::string play_command = "play "+deviceName;
 
-    //用mciSendStringW()函数，第一个参数写StoW(播放命令).c_str()
+    //声明打开命令
+    std::string open_command = "open \"" + music_address + "\" alias " + deviceName;
+
+    //打开音乐文件
+    mciSendString(open_command.c_str(), NULL, 0, NULL);
+
+    //声明播放命令（只要在音乐路径前加一个play 就行注意，注意，注意，这里的play后有一个空格，一定要加！！！）
+    std::string play_command = "play "+deviceName + " repeat";//后面可以加上repeat等状态
+
+    //播放音乐
     mciSendString(play_command.c_str(),NULL,0,NULL);
 
     start();
